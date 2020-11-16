83a938732dfd0dff75d37283467c44a52546accb
tejitpabari99 <tvp2107@columbia.edu>
2020-11-16 05:56:25 +0530

    index.html changes

--- application.py
+++ application.py
@@ -61,40 +61,40 @@ def tutors_id(idTutors):
     data = td.get_tutors_by_id(idTutors=idTutors, params=custParams, url=request.url_root)
     rsp = Response(json.dumps(data), status=200, content_type="application/json")
     return rsp
-
-@application.route("/tutors/add", methods=["GET"])
-def tutors_add():
-    params = request.args.copy()
-    try:
-        data = td.add_tutor(params=params)
-        rsp = Response(json.dumps(data), status=200, content_type="application/json")
-    except Exception as e:
-        print('Error:',e)
-        rsp = Response('Error: {}'.format(str(e)), status=400)
-    return rsp
-
-@application.route("/tutors/delete/<usrEmail>", methods=["GET"])
-def tutors_delete(usrEmail):
-    email = True if '@' in usrEmail else False
-    try:
-        data = td.delete_tutor(usrEmail,email)
-        rsp = Response(json.dumps(data), status=200, content_type="application/json")
-    except Exception as e:
-        print('Error:',e)
-        rsp = Response('Error: {}'.format(str(e)), status=400)
-    return rsp
-
-@application.route("/tutors/update/<usrEmail>", methods=["GET"])
-def tutors_update(usrEmail):
-    email = True if '@' in usrEmail else False
-    params = request.args.copy()
-    try:
-        data = td.update_tutor(usrEmail,email, params)
-        rsp = Response(json.dumps(data), status=200, content_type="application/json")
-    except Exception as e:
-        print('Error:',e)
-        rsp = Response('Error: {}'.format(str(e)), status=400)
-    return rsp
+# 
+# @application.route("/tutors/add", methods=["GET"])
+# def tutors_add():
+#     params = request.args.copy()
+#     try:
+#         data = td.add_tutor(params=params)
+#         rsp = Response(json.dumps(data), status=200, content_type="application/json")
+#     except Exception as e:
+#         print('Error:',e)
+#         rsp = Response('Error: {}'.format(str(e)), status=400)
+#     return rsp
+#
+# @application.route("/tutors/delete/<usrEmail>", methods=["GET"])
+# def tutors_delete(usrEmail):
+#     email = True if '@' in usrEmail else False
+#     try:
+#         data = td.delete_tutor(usrEmail,email)
+#         rsp = Response(json.dumps(data), status=200, content_type="application/json")
+#     except Exception as e:
+#         print('Error:',e)
+#         rsp = Response('Error: {}'.format(str(e)), status=400)
+#     return rsp
+#
+# @application.route("/tutors/update/<usrEmail>", methods=["GET"])
+# def tutors_update(usrEmail):
+#     email = True if '@' in usrEmail else False
+#     params = request.args.copy()
+#     try:
+#         data = td.update_tutor(usrEmail,email, params)
+#         rsp = Response(json.dumps(data), status=200, content_type="application/json")
+#     except Exception as e:
+#         print('Error:',e)
+#         rsp = Response('Error: {}'.format(str(e)), status=400)
+#     return rsp
 
 if __name__ == "__main__":
     application.run(debug=True)
